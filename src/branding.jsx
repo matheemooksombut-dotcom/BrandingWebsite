3a2e2584da413246585fdd94e1ba418b66de23d6
matheemooksombut-dotcom <matheemooksombut@gmail.com>
2026-03-09 23:17:56 +0700

    1

--- src/branding.jsx
+++ src/branding.jsx
@@ -3,6 +3,11 @@ import branding1 from './assets/branding1.png'
 import branding2 from './assets/branding2.png'
 import branding3 from './assets/branding3.png'
 import branding4 from './assets/branding4.png'
+import box from './assets/box-solid-full.svg'
+import cart from './assets/cart-arrow-down-solid-full.svg'
+import pay from './assets/amazon-brands-solid-full.svg'
+
+
 
 
 function Branding() {
@@ -25,6 +30,30 @@ function Branding() {
                  <img src={branding4} alt="" />
             </div>
         </div>
+
+      <div className='content-branding'>
+        
+      </div>
+
+
+       <h1 className='branding-font'>วิธีการสั่งซื้อ</h1>
+        <div className="news-section">
+          <div className="news-item">
+            <img className="icon-img" src={box} alt="" />
+            <p>1. เลือกสินค้าที่ต้องการซื้อ</p>
+          </div>
+
+          <div className="news-item">
+            <img className="icon-img" src={cart} alt="" />
+            <p>2. ใส่สินค้าลงกล่องส่ง</p>
+          </div>
+
+          <div className="news-item">
+            <img className="icon-img" src={pay} alt="" />
+            <p>3. เลือกช่องทางการชำระเงิน</p>
+          </div>
+        </div>
+
     </>
   )
 }
