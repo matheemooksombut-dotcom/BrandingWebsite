b1cc4d81544790b1c1738154ba54c9ad50d236a6
matheemooksombut-dotcom <matheemooksombut@gmail.com>
2026-03-14 16:06:52 +0700

    font-end

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import { Routes, Route } from "react-router-dom"
 import Home from "./Home.jsx"
 import Items from "./items.jsx"
 import Product1 from "./product-1.jsx"
+import Product2 from "./product-2.jsx"
+import Product3 from "./product-3.jsx"
+import Product4 from "./product-4.jsx"
 
 
 function App() {
@@ -10,6 +13,9 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/items" element={<Items />} />
       <Route path="/product-1" element={<Product1 />} />
+      <Route path="/product-2" element={<Product2 />} />
+      <Route path="/product-3" element={<Product3 />} />
+      <Route path="/product-4" element={<Product4 />} />
     </Routes>
   )
 }
--- src/product-4.jsx
+++ src/product-4.jsx
@@ -1,16 +1,112 @@
 import '../src/style/index.css'
 import Navbar from "./components/navbar.jsx"
 import Footer from './components/footer.jsx'
-import Itemscompoun from './components/itemscompoun.jsx'
-function Items() {
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+function Product3() {
+  const [selected, setSelected] = useState("");
   return (
     <>
       <Navbar />
-      <Itemscompoun />
-      <div id="contact">
-        <Footer />
+
+     <div className="flex-product">
+          <div className="Product1-section">
+
+            <div className="Product1-img">
+                <img src="../../public/box4.png" alt="" />
+            </div>
+          </div>
+
+      </div> 
+
+      
+      <div className='box-products-wrapper'>
+          <div className="content-box-products">
+              <div className='Content-product'>
+                <h2>กล่องประจำเดือน</h2>
+                <p>SKU:002</p>
+                <p>นี่คือคำอธิบายสินค้า ที่ตรงนี้เหมาะอย่างยิ่งสำหรับ “ขาย” สินค้าของคุณและดึงดูดความสนใจของผู้ซื้อ อธิบายสินค้าของคุณอย่างกระชับและชัดเจน ใช้คีย์เวิร์ดเฉพาะ เขียนคำอธิบายของคุณเองแทนที่จะใช้คำโฆษณาจากผู้ผลิต</p>
+                <br />
+                
+                <div className='Data-product'>
+                    <b><p>ข้อมูลสินค้า</p></b>
+                   <b> <p>นโยบายสินค้า</p></b>
+                   <b> <p>ข้อมูลการจัดส่ง</p></b>
+                </div>
+              </div>
+          </div>
+          <br />
+
+          <div className='Details-Products'>
+              <h2>฿1,500.00</h2>
+              <p>รายการราคา *</p>
+              <label className="price-box">
+
+                 <div className='option-1'>
+                    <input type='radio' name='price' value="once" checked={selected ==="once"}
+                    onChange={(e) => setSelected(e.target.value)} />
+                    <br />
+                      <div>
+                  <div>ซื้อแบบครั้งเดียว</div>
+                    <div className="price">฿1,500.00</div>
+                  </div>
+
+                    
+                </div>
+               
+                <div className='option-2'>
+                  <input type='radio' name='price' value="once" checked={selected ==="once"}
+                  onChange={(e) => setSelected(e.target.value)} />
+                  <br />
+                    <div>
+                <div>3 เดือน</div>
+                
+                  <div className="price">฿1,500.00
+                    
+                    <br /><span>ทุกๆ 3 เดือน</span>
+                  </div>
+                  
+                </div>
+
+                  
+                </div>
+
+                <div className='option-3'>
+                  <input type='radio' name='price' value="once" checked={selected ==="once"}
+                  onChange={(e) => setSelected(e.target.value)} />
+                  <br />
+                    <div>
+                <div>6 เดือน </div>
+                  <div className="price">฿1,500.00
+                    <br /><span>  ทุกๆ 6 เดือน</span></div>
+                </div>
+
+                  
+                </div>
+               
+              </label>
+          </div>
+
+
+               
+
+
+
+
+                
+          
       </div>
+      
+      
+        <div id="contact">
+          <Footer />
+        </div>
+
+
+
+
+
     </>
   )
 }
-export default Items
+export default Product3
